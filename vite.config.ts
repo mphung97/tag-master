fa53cd1e6f0c6f8fd43e585e37fe8c612e4c61f4
mphung97 <mphung97@github.com>
2024-05-29 10:04:41 +0700

    feat: add react-compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      babel: {
+        presets: [],
+        // Your plugins run before any built-in transform (eg: Fast Refresh)
+        plugins: [],
+        // Use .babelrc files
+        babelrc: true,
+        // Use babel.config.js files
+        configFile: true,
+      },
+    }),
+  ],
+});
